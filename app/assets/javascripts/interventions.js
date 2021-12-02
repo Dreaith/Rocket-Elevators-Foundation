5bd31e78b09080361c6721ecdec021d7ef58dd90
dreaith <noel80@hotmail.ca>
2021-12-02 15:08:00 -0500

    table filtered

--- app/assets/javascripts/interventions.js
+++ app/assets/javascripts/interventions.js
@@ -1,28 +1,28 @@
-$("#building_id").prop("disabled", true); // second dropdown is disable while first dropdown is empty
-$("#customer_id").change(function(){
-  	var project = $(this).val();
-  	if(project == ''){
-  		$("#building_id").prop("disabled", true);
-  	}else{
-  		$("#building_id").prop("disabled", false);
-  	}
-  	$.ajax({
-	    url: "/interventions",
-	    method: "GET",  
-	    dataType: "json",
-	    data: {getBuilding: customer_id},
-	    error: function (xhr, status, error) {
-	      	console.error('AJAX Error: ' + status + error);
-	    },
-	    success: function (response) {
-	      	console.log(response);
-	      	var buildings = response["buildings"];
-	      	$("#building_id").empty();
+// $("#intervention_customer_id").on("change", function(){
+//   $.ajax({
+//     url: "/interventions/get_building",
+//     method: "GET",
+    
+//     dataType: "json",
+//     data: {get_building: customer_id},
+//     error: function (xhr, status, error) {
+//         console.error('AJAX Error: ' + status + error);
+//     },
+//     success: function (response) {
+//         console.log(response);
+//         $("#intervention_customer_id").val()
+//         var buildings = response['get_building'];
+//         console.log(response)
+//         $("#intervention_building_id").
 
-	      	$("#building_id").append('<option>Select Building</option>');
-	      	for(var i = 0; i < buildings.length; i++){
-	      		$("#building_id").append('<option value="' + buildings[i]["id"] + '">' +buildings[i]["full_name_of_the_building_administrator"] + '</option>');
-	      	}
-	    }
-  	});
-});
+//         $("#intervention_building_id").append('<option>Select Building</option>');
+//         for(var i = 0; i < buildings.length; i++){
+//             $("#intervention_building_id").append("<option value=" + buildings[i].id + '>' +buildings[i].id + '</option>');
+//         }
+//     }
+// }
+// );
+
+// console.log('ok');
+  
+// })
--- app/controllers/interventions_controller.rb
+++ app/controllers/interventions_controller.rb
@@ -1,9 +1,39 @@
 class InterventionsController < InheritedResources::Base
 
+  def trueAuthor
+    Interventions.author = current_user.first_name+" "+current_user.last_name
+  end
+
+  def get_building
+    @building = Building.where(customer_id: params[:building_id])
+    puts '----------------------------- ok ------------------------'
+    render json: {buildings: @building}
+  end
+
+  def get_battery
+    @battery = Battery.where(building_id: params[:battery_id])
+    puts @battery
+    render json: {batteries: @battery}
+  end
+
+  def get_column
+    @column = Column.where(battery_id: params[:column_id])
+
+    render json: {columns: @column}
+  end
+
+  def get_elevator
+    @elevator = Elevator.where(column_id: params[:elevator_id])
+
+    render json:{elevators: @elevator}
+  end
+  
   private
 
     def intervention_params
-      params.require(:intervention).permit(:author, :intervention_customer_id, :intervention_building_id, :intervention_battery_id, :intervention_column_id, :intervention_elevator_id, :intervention_employee_id)
+      params.require(:intervention).permit(:author, :customer_id, :building_id, :battery_id, :column_id, :elevator_id, :employee_id,:report)
     end
+
+   
 
 end
