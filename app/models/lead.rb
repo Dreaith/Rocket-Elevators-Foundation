6c0d6675918c5c912c54073ef8ea009f67c66239
dreaith <noel80@hotmail.ca>
2022-01-31 14:54:39 -0500

    db change

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,20 +1,8 @@
 class Lead < ApplicationRecord
     has_one_attached :contact_attachment_file
-    before_create :sanitize_filename
-    after_create :upload
     def attachment_path
     
         
-    end
-    def sanitize_filename(filename)
-        filename.strip.tap do |name|
-          # NOTE: File.basename doesn't work right with Windows paths on Unix
-          # get only the filename, not the whole path
-          name.sub! /\A.*(\\|\/)/, ''
-          # Finally, replace all non alphanumeric, underscore
-          # or periods with underscore
-          name.gsub! /[^\w\.\-]/, '_'
-        end
     end
 
     def to_s
